339bf9b32276ceada8db8d8d099eacb00513c699
Michael <enjoyweaver@gmail.com>
2024-01-31 13:42:18 -0600

    intro

--- components/Body.tsx
+++ components/Body.tsx
@@ -17,11 +17,16 @@ export default function Body() {
           My Calendy is designed to organize your tasks and activities based on
           your cognitive abilities, enhancing your efficiency and productivity.
         </p>
-        <p className="text-1.5xl !leading-tight mx-auto max-w-xl text-center pt-10 ">
-          Are you getting the most out of yourself and your day by aligning your
-          schedule when your cognitive abilities are at their peak? Everyone is
-          different so is when their cognitive abilities are at their peak, so
-          that is
+        <p className="text-1.5xl !leading-tight mx-auto max-w-xl text-center pt-10">
+          My Calendy doesn't just tell you <b>when</b> to do something, it tells
+          you <b>what</b> to do <b>when</b>. By understanding your fluctuating
+          cognitive <b>abilities</b>, it aligns your tasks <b>accordingly</b>.
+          Need <b>analytical thinking</b>? My Calendy schedules that for when
+          your brain is <b>sharpest</b>.<b> Creative writing</b>? It plans that
+          for when your <b>imagination</b> is soaring. It's like having a
+          personal productivity coach whispering in your ear, guiding you
+          towards
+          <b> peak performance</b> hour by hour.
         </p>
       </main>
       <Columns />
