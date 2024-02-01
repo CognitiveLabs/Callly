581dea19efd6cdd5fd4b12bf704c6931575e3592
Michael <enjoyweaver@gmail.com>
2024-01-31 22:15:04 -0600

    small details

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,5 @@
+import BlogPost from './post1';
+
 export default function Blog() {
   return (
     <div className="blog-page">
@@ -10,8 +12,12 @@ export default function Blog() {
           Insights for Aligning Your Calendar with Your Mind
         </p>
       </header>
-
-      <main className="blog-posts"></main>
+      <br />
+      <br />
+      <p className="text-2xl signika-orange text-orange-500 font-extrabold sm:text-center">
+        Our first blog post
+      </p>
+      <BlogPost />
 
       <h3 className="text-xl font-bold mb-4">Explore the Blog</h3>
     </div>
---
+++ app/blog/post1.tsx
@@ -0,0 +1,96 @@
+// BlogPost.tsx
+
+import React from 'react';
+
+const BlogPost: React.FC = () => {
+  return (
+    <div className="blog mx-auto my-10">
+      <h2 className="text-2xl signika-header font-extrabold sm:text-left mb-3">
+        Exploring the Dynamics of Cognitive Abilities and Productivity Over Time
+      </h2>
+
+      {/* Introduction */}
+      <p className="text-lg mb-6">
+        Welcome to our journey into the world of cognitive research and
+        scheduling efficiency. In this blog post, we'll unravel the connections
+        between cognitive abilities, time of day, and individual productivity,
+        drawing insights from three key studies.
+      </p>
+
+      {/* Unraveling the Complexity of Individual Cognitive Structures */}
+      <h2 className="text-2xl signika-header font-extrabold sm:text-left mb-3">
+        The Complexity of Individual Cognitive Structures
+      </h2>
+      <p className=" mb-6">
+        Our journey begins with a groundbreaking study by Florian Schmiedek and
+        his team at DIPF | Leibniz Institute for Research and Information in
+        Education. Over six months, they explored the cognitive abilities of 101
+        young adults, challenging the consensus that human cognitive abilities
+        are hierarchically organized.
+      </p>
+      <p className=" mb-6">
+        Working memory emerges as a key contributor to both between- and
+        within-person cognitive structures, emphasizing its pivotal role in
+        understanding the nuances of cognitive abilities within individuals
+        <span className="footnote">[1]</span>.
+      </p>
+      <h2 className="text-2xl signika-header font-extrabold sm:text-left mb-3">
+        The Influence of Time-of-Day on Cognitive Performance
+      </h2>
+      <p>
+        Shifting our focus to the impact of time-of-day on cognitive
+        performance, Denni Tommasi from Monash University explores the relevance
+        of time-of-day effects on efficiency gains in cognitive tasks.
+        Productivity varies not only based on the time of day but also on the
+        type of task being performed.
+      </p>
+      <br />
+      <p className="mb-6">
+        Understanding the fluctuations in alertness and mental focus throughout
+        the day provides insights that reshape how we approach task sorting for
+        skilled individuals within an organization
+        <span className="footnote">[2]</span>.
+      </p>
+
+      <h2 className="text-2xl signika-header font-extrabold sm:text-left mb-3">
+        Chronotype and Diurnal Performance Profiles
+      </h2>
+      <p>
+        Our exploration concludes with a study by Elise R. Facer-Childs and her
+        colleagues, delving into the effects of time of day and chronotype on
+        cognitive and physical performance. Whether you're a morning lark or a
+        night owl significantly contributes to the timing of peak athletic
+        performance.
+      </p>
+      <br />
+      <p className="mb-6">
+        Understanding these chronobiological factors can provide valuable
+        insights into optimizing cognitive and physical tasks, considering
+        individual preferences and peak performance periods
+        <span className="footnote">[3]</span>.
+      </p>
+
+      {/* Conclusion */}
+      <h2 className="text-2xl signika-header font-extrabold sm:text-left mb-3">
+        Conclusion
+      </h2>
+      <p className="mb-6">
+        As we navigate through these studies, the intricate connections between
+        cognitive abilities, time of day, and productivity become evident. These
+        findings encourage a nuanced approach to understanding individual
+        differences and optimizing performance within the constraints of time.
+        My Calendy, the Cognitive Calendar, strives to incorporate these
+        insights, guiding users toward peak cognitive performance by aligning
+        tasks with their unique cognitive structures and the optimal times of
+        day.
+      </p>
+      <p>
+        In the realm of cognitive optimization, the journey continues, with each
+        study contributing to the rich tapestry of knowledge that shapes how we
+        perceive and harness our cognitive potential.
+      </p>
+    </div>
+  );
+};
+
+export default BlogPost;
