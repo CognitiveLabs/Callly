dacfe4c53cbc973561a9561d53203bf0131ab19b
Michael <enjoyweaver@gmail.com>
2024-01-31 20:38:46 -0600

    blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,19 @@
+export default function Blog() {
+  return (
+    <div className="blog-page">
+      <header>
+        <h1 className="text-4xl signika-header font-extrabold sm:text-center sm:text-6xl">
+          My Calendy Blog
+        </h1>
+        <br />
+        <p className="text-2xl signika-title font-extrabold sm:text-center">
+          Insights for Aligning Your Calendar with Your Mind
+        </p>
+      </header>
+
+      <main className="blog-posts"></main>
+
+      <h3 className="text-xl font-bold mb-4">Explore the Blog</h3>
+    </div>
+  );
+}
