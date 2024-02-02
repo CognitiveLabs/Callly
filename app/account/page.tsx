595d9f71d6328ff322fd07e3e2993f995647d5fb
Michael <enjoyweaver@gmail.com>
2024-02-01 19:24:21 -0600

    account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -69,14 +69,20 @@ export default async function Account() {
   async function createCalendarEvent(eventData: any) {
     const supabase = createServerActionClient<Database>({ cookies });
     const session = await getSession();
-    const token = await session.user.provider_token.google.access_token;
 
-    const calendar = google.calendar({ version: 'v3', auth: token });
+    // Use optional chaining to safely access potentially null properties
+    const token = await session?.user?.provider_token?.google?.access_token;
+
+    if (!token) {
+      // Handle the case where there's no token (e.g., user not signed in)
+      return; // Or display an error message
+    }
 
     try {
+      const calendar = google.calendar({ version: 'v3', auth: token });
       const event = await calendar.events.insert({
-        calendarId: 'primary' // Insert into user's primary calendar
-        // ...eventData (event details)
+        calendarId: 'primary', // Insert into user's primary calendar
+        ...eventData // Event details
       });
       console.log('Event created:', event.data);
       // Handle success (e.g., display a success message)
@@ -90,12 +96,9 @@ export default async function Account() {
     <section className="mb-32 bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
         <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Account
+          <h1 className="text-4xl signika-header font-extrabold text-white sm:text-center sm:text-6xl">
+            Your Account
           </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
-          </p>
         </div>
       </div>
       <div className="p-4">
